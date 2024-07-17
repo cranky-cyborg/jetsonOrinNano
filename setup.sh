79766f53bbb0f5d34a555cab42e265d6cf123fda
Cranky Cyborg <4370667+cranky-cyborg@users.noreply.github.com>
2024-07-17 19:10:27 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,22 @@ PWD=$(pwd)
 mkdir $HOME/nvidia
 cd $HOME/nvidia
 
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install --upgrade setuptools
+python3.10 -m pip install --upgrade wheel
+python3.10 -m pip install virtualenv
+sudo apt install python3.10-venv
+python3.10 -m venv myENV --system-site-packages
+source ~/myENV/bin/activate
+cd myENV
+
+
 #manually check if this is the latest
 wget https://developer.download.nvidia.com/compute/redist/jp/v60/pytorch/torch-2.4.0a0+f70bd71a48.nv24.06.15634931-cp310-cp310-linux_aarch64.whl
 
 wget https://developer.download.nvidia.com/compute/redist/jp/v60/tensorflow/tensorflow-2.16.1+nv24.06-cp310-cp310-linux_aarch64.whl
 
-pip install --upgrade pip
+
 
 sudo apt install libhdf5-dev
 
