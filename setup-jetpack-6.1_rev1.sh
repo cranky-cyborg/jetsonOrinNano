0d02430e1ad51a04879254cc6266d9cc27597933
Cranky Cyborg <4370667+cranky-cyborg@users.noreply.github.com>
2025-01-12 20:09:17 +1300

    Create setup-jetpack-6.1_rev1.sh

---
+++ setup-jetpack-6.1_rev1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#below line suppresses requests for sudo passwords
+echo " 
+${USER} ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo;
+
+#capturing the current working directory
+PWD=$(pwd)
+
+#update packages
+apt update 
+apt upgrade
+
+#installing pip
+apt install python3-pip
+apt install python3-venv
